49114142f8de283f0824e96e70341608c309f805
Ryo Takahashi <ryogiftjp@gmail.com>
2021-12-19 18:49:11 +0900

    Add content validates of post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:one)
+  end
+
+  test "有効であること" do
+    assert @post.valid?
+  end
+
+  test "投稿内容が必須であること" do
+    @post.content = ""
+    @post.valid?
+    assert_includes @post.errors[:content], I18n.t("errors.messages.blank")
+  end
+
+  test "投稿内容が1000文字いないであること" do
+    @post.content = "a" * 1001
+    @post.valid?
+    assert_includes @post.errors[:content], I18n.t("errors.messages.too_long", count: 1000)
+  end
 end
